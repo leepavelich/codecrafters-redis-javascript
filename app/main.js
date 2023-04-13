b2b6bd32558ecdb6cb525be033dd6b13f7b60cfe
Lee Pavelich <leepavelich@gmail.com>
2023-04-13 12:53:08 -0600

    Handle echo

--- app/main.js
+++ app/main.js
@@ -3,7 +3,7 @@ const net = require("net");
 let DEV_MODE = false;
 
 // comment out this line before submitting
-// DEV_MODE = true;
+DEV_MODE = true;
 
 const PORT = DEV_MODE ? 6380 : 6379;
 
@@ -11,7 +11,20 @@ const server = net.createServer((connection) => {
   console.log("Client connected.");
 
   connection.on("data", (data) => {
-    connection.write("+PONG\r\n");
+    const request = data.toString().trim().split("\r\n");
+    const command = request[2];
+    switch (command) {
+      case "COMMAND":
+        connection.write("$-1\r\n");
+        break;
+      case "ping":
+        connection.write("+PONG\r\n");
+        break;
+      case "ECHO":
+        const response = request[4];
+        connection.write(`$${response.length}\r\n${response}\r\n`);
+        break;
+    }
   });
 });
 
