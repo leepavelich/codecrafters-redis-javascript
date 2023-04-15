0c8c1e533c7dd3308439f4e412c67f4b5c7d5598
Lee Pavelich <leepavelich@gmail.com>
2023-04-15 12:41:10 -0600

    parseResp function

--- app/main.js
+++ app/main.js
@@ -2,12 +2,12 @@ const net = require("net");
 const { PORT, ENV } = require("./config");
 const handleCommand = require("./commands");
 
+const parseResp = (data) => data.toString().split(/[\r\n]+/);
+
 const server = net.createServer((connection) => {
   if (ENV !== "test") console.log("Client connected.");
 
-  connection.on("data", (data) =>
-    handleCommand(connection, data.toString().split(/[\r\n]+/))
-  );
+  connection.on("data", (data) => handleCommand(connection, parseResp(data)));
 });
 
 server.listen(PORT, "127.0.0.1");
