c5b70f2350f343351c885dc7676b9e6d90f5892b
Lee Pavelich <leepavelich@gmail.com>
2023-04-14 18:32:32 -0600

    Refactoring

--- app/commands.js
+++ app/commands.js
@@ -1,48 +1,27 @@
 const store = {};
 
 const handleCommand = (connection, request) => {
-  const [, , commandRaw, , arg1, , arg2] = request;
-  const command = commandRaw.toUpperCase();
+  const [, , respCommand, , key, , value] = request;
+  const sendResponse = (response) => connection.write(`${response}\r\n`);
+  const sendNullResponse = () => sendResponse("$-1");
 
   const commandHandlers = {
-    COMMAND: () => handleCommandCommand(connection),
-    PING: () => handlePingCommand(connection),
-    ECHO: () => handleEchoCommand(connection, arg1),
-    SET: () => handleSetCommand(connection, arg1, arg2),
-    GET: () => handleGetCommand(connection, arg1),
+    COMMAND: sendNullResponse,
+    PING: () => sendResponse("+PONG"),
+    ECHO: () => sendResponse(`$${key.length}\r\n${key}`),
+    SET: () => {
+      store[key] = value;
+      sendResponse("+OK");
+    },
+    GET: () => {
+      const response = store[key];
+      sendResponse(response ? `$${response.length}\r\n${response}` : "$-1");
+    },
   };
 
-  const commandHandler = commandHandlers[command];
+  const command = respCommand.toUpperCase();
 
-  if (commandHandlers[command]) {
-    commandHandler();
-  } else {
-    connection.write(`-ERR unknown command '${command}'\r\n`);
-  }
-};
-
-const handleCommandCommand = (connection) => {
-  connection.write("$-1\r\n");
-};
-
-const handlePingCommand = (connection) => {
-  connection.write("+PONG\r\n");
-};
-
-const handleEchoCommand = (connection, message) => {
-  connection.write(`$${message.length}\r\n${message}\r\n`);
-};
-
-const handleSetCommand = (connection, key, value) => {
-  store[key] = value;
-  connection.write("+OK\r\n");
-};
-
-const handleGetCommand = (connection, key) => {
-  const response = store[key];
-  connection.write(
-    response ? `$${response.length}\r\n${response}\r\n` : "$-1\r\n"
-  );
+  return (commandHandlers[command] || sendNullResponse)();
 };
 
 module.exports = handleCommand;
